19443021994d9a03e3c687b4fc329a8469e12238
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2023-01-06 14:27:40 +0100

    initial commit

---
+++ packages/fork-tests/tests/20221113-euler-rebalanced-linear-pool/readme.md
@@ -0,0 +1,5 @@
+# 2022-11-13 - Euler Rebalanced Linear Pool
+
+Deployment of the `EulerLinearPoolFactory`, for Linear Pools with a Euler Token (eToken).
+
+## Useful Files
---
+++ packages/linear-pools/contracts/euler-linear-pool/EulerLinearPool.test.ts
@@ -0,0 +1,228 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { Contract } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+
+import { bn, fp } from '@orbcollective/shared-dependencies/numbers';
+import {
+  deployPackageContract,
+  getPackageContractDeployedAt,
+  deployToken,
+  setupEnvironment,
+  getBalancerContractArtifact,
+  MAX_UINT256,
+  ZERO_ADDRESS,
+} from '@orbcollective/shared-dependencies';
+
+import { MONTH } from '@orbcollective/shared-dependencies/time';
+
+import * as expectEvent from '@orbcollective/shared-dependencies/expectEvent';
+import TokenList from '@orbcollective/shared-dependencies/test-helpers/token/TokenList';
+
+export enum SwapKind {
+  GivenIn = 0,
+  GivenOut,
+}
+
+enum RevertType {
+  DoNotRevert,
+  NonMalicious,
+  MaliciousSwapQuery,
+  MaliciousJoinExitQuery,
+}
+
+async function deployBalancerContract(
+  task: string,
+  contractName: string,
+  deployer: SignerWithAddress,
+  args: unknown[]
+): Promise<Contract> {
+  const artifact = await getBalancerContractArtifact(task, contractName);
+  const factory = new ethers.ContractFactory(artifact.abi, artifact.bytecode, deployer);
+  const contract = await factory.deploy(...args);
+
+  return contract;
+}
+
+describe('EulerLinearPool', function () {
+  let pool: Contract;
+  let vault: Contract;
+  let tokens: TokenList;
+  let mainToken: Contract, wrappedToken: Contract, wrappedTokenInstance: Contract;
+  let poolFactory: Contract;
+  let guardian: SignerWithAddress, lp: SignerWithAddress, owner: SignerWithAddress;
+  let manager: SignerWithAddress;
+
+  // Euler Mainnet
+  const EULER_PROTOCOL = '0x27182842E098f60e3D576794A5bFFb0777E025d3';
+  const POOL_SWAP_FEE_PERCENTAGE = fp(0.01);
+
+  const EULER_PROTOCOL_ID = 0;
+
+  const BASE_PAUSE_WINDOW_DURATION = MONTH * 3;
+  const BASE_BUFFER_PERIOD_DURATION = MONTH;
+
+  before('setup', async () => {
+    let deployer: SignerWithAddress;
+    let trader: SignerWithAddress;
+
+    // appease the @typescript-eslint/no-unused-vars lint error
+    [, lp, owner] = await ethers.getSigners();
+    ({ vault, deployer, trader } = await setupEnvironment());
+    manager = deployer;
+    guardian = trader;
+
+    // Deploy tokens
+    mainToken = await deployToken('USDC', 18, deployer);
+    wrappedTokenInstance = await deployPackageContract('MockEulerToken', {
+      args: ['cDAI,', 'cDAI', 18, mainToken.address],
+    });
+
+    wrappedToken = await getPackageContractDeployedAt('TestToken', wrappedTokenInstance.address);
+
+    tokens = new TokenList([mainToken, wrappedToken]).sort();
+
+    await tokens.mint({ to: [lp, trader], amount: fp(100) });
+
+    // Deploy Balancer Queries
+    const queriesTask = '20220721-balancer-queries';
+    const queriesContract = 'BalancerQueries';
+    const queriesArgs = [vault.address];
+    const queries = await deployBalancerContract(queriesTask, queriesContract, manager, queriesArgs);
+
+    // Deploy poolFactory
+    poolFactory = await deployPackageContract('EulerLinearPoolFactory', {
+      args: [
+        vault.address,
+        ZERO_ADDRESS,
+        queries.address,
+        'factoryVersion',
+        'poolVersion',
+        BASE_PAUSE_WINDOW_DURATION,
+        BASE_BUFFER_PERIOD_DURATION,
+        EULER_PROTOCOL,
+      ],
+    });
+
+    // Deploy and initialize pool
+    const tx = await poolFactory.create(
+      'Balancer Pool Token',
+      'BPT',
+      mainToken.address,
+      wrappedToken.address,
+      bn(0),
+      POOL_SWAP_FEE_PERCENTAGE,
+      owner.address,
+      EULER_PROTOCOL_ID
+    );
+    const receipt = await tx.wait();
+    const event = expectEvent.inReceipt(receipt, 'PoolCreated');
+
+    pool = await getPackageContractDeployedAt('LinearPool', event.args.pool);
+  });
+
+  describe('constructor', () => {
+    it('reverts if the mainToken is not the ASSET of the wrappedToken', async () => {
+      const otherToken = await deployToken('USDC', 18, manager);
+
+      await expect(
+        poolFactory.create(
+          'Balancer Pool Token',
+          'BPT',
+          otherToken.address,
+          wrappedToken.address,
+          bn(0),
+          POOL_SWAP_FEE_PERCENTAGE,
+          owner.address,
+          EULER_PROTOCOL_ID
+        )
+      ).to.be.revertedWith('BAL#520'); // TOKEN_MISMATCH code
+    });
+  });
+
+  describe('asset managers', async () => {
+    it('sets the same asset manager for main and wrapped token', async () => {
+      const poolId = await pool.getPoolId();
+
+      const { assetManager: firstAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.first.address);
+      const { assetManager: secondAssetManager } = await vault.getPoolTokenInfo(poolId, tokens.second.address);
+
+      expect(firstAssetManager).to.not.equal(ZERO_ADDRESS);
+      expect(firstAssetManager).to.equal(secondAssetManager);
+    });
+
+    it('sets the no asset manager for the BPT', async () => {
+      const poolId = await pool.getPoolId();
+      const { assetManager } = await vault.getPoolTokenInfo(poolId, pool.address);
+      expect(assetManager).to.equal(ZERO_ADDRESS);
+    });
+  });
+
+  describe('getWrappedTokenRate', () => {
+    context('under normal operation', () => {
+      it('returns the expected value', async () => {
+        expect(await pool.getWrappedTokenRate()).to.be.eq(bn(1e18));
+
+        // change exchangeRate at the EulerToken
+        await wrappedTokenInstance.setExchangeRateMultiplier(2);
+        expect(await pool.getWrappedTokenRate()).to.be.eq(bn(2e18));
+
+        // change exchangeRate at the EulerToken
+        await wrappedTokenInstance.setExchangeRateMultiplier(1);
+        expect(await pool.getWrappedTokenRate()).to.be.eq(bn(1e18));
+      });
+    });
+
+    context('when Euler reverts maliciously to impersonate a swap query', () => {
+      it('reverts with MALICIOUS_QUERY_REVERT', async () => {
+        await wrappedTokenInstance.setRevertType(RevertType.MaliciousSwapQuery);
+        await expect(pool.getWrappedTokenRate()).to.be.revertedWith('BAL#357');
+      });
+    });
+
+    context('when Euler reverts maliciously to impersonate a join/exit query', () => {
+      it('reverts with MALICIOUS_QUERY_REVERT', async () => {
+        await wrappedTokenInstance.setRevertType(RevertType.MaliciousJoinExitQuery);
+        await expect(pool.getWrappedTokenRate()).to.be.revertedWith('BAL#357');
+      });
+    });
+  });
+
+  describe('rebalancing', () => {
+    context('when Euler reverts maliciously to impersonate a swap query', () => {
+      let rebalancer: Contract;
+      beforeEach('provide initial liquidity to pool', async () => {
+        await wrappedTokenInstance.setRevertType(RevertType.DoNotRevert);
+        const poolId = await pool.getPoolId();
+        await tokens.approve({ to: vault, amount: fp(100), from: lp });
+        await vault.connect(lp).swap(
+          {
+            poolId,
+            kind: SwapKind.GivenIn,
+            assetIn: mainToken.address,
+            assetOut: pool.address,
+            amount: fp(10),
+            userData: '0x',
+          },
+          { sender: lp.address, fromInternalBalance: false, recipient: lp.address, toInternalBalance: false },
+          0,
+          MAX_UINT256
+        );
+      });
+
+      beforeEach('deploy and initialize pool', async () => {
+        const poolId = await pool.getPoolId();
+        const { assetManager } = await vault.getPoolTokenInfo(poolId, tokens.first.address);
+        rebalancer = await getPackageContractDeployedAt('EulerLinearPoolRebalancer', assetManager);
+      });
+
+      beforeEach('make Euler lending pool start reverting', async () => {
+        await wrappedTokenInstance.setRevertType(RevertType.MaliciousSwapQuery);
+      });
+
+      it('reverts with MALICIOUS_QUERY_REVERT', async () => {
+        await expect(rebalancer.rebalance(guardian.address)).to.be.revertedWith('BAL#357'); // MALICIOUS_QUERY_REVERT
+      });
+    });
+  });
+});
