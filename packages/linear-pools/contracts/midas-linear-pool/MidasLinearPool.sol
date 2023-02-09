ef1b5e18f02c5f7f8b97145d3170545d4fec7995
mkflow27 <35266877+mkflow27@users.noreply.github.com>
2023-02-09 13:34:26 +0100

    add midas linear pool

---
+++ packages/linear-pools/contracts/midas-linear-pool/MidasLinearPool.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-pool-utils/contracts/lib/ExternalCallLib.sol";
+import "@balancer-labs/v2-pool-utils/contracts/Version.sol";
+import "./interfaces/ICToken.sol";
+
+import "@balancer-labs/v2-pool-linear/contracts/LinearPool.sol";
+
+contract MidasLinearPool is LinearPool, Version {
+    ICToken private immutable _cToken;
+
+    struct ConstructorArgs {
+        IVault vault;
+        string name;
+        string symbol;
+        IERC20 mainToken;
+        IERC20 wrappedToken;
+        address assetManager;
+        uint256 upperTarget;
+        uint256 swapFeePercentage;
+        uint256 pauseWindowDuration;
+        uint256 bufferPeriodDuration;
+        address owner;
+        string version;
+    }
+
+    constructor(ConstructorArgs memory args)
+        LinearPool(
+            args.vault,
+            args.name,
+            args.symbol,
+            args.mainToken,
+            args.wrappedToken,
+            args.upperTarget,
+            _toAssetManagerArray(args),
+            args.swapFeePercentage,
+            args.pauseWindowDuration,
+            args.bufferPeriodDuration,
+            args.owner
+        )
+        Version(args.version)
+    {
+        ICToken cToken = ICToken(address(args.wrappedToken));
+
+        _cToken = cToken;
+
+        _require(address(args.mainToken) == cToken.underlying(), Errors.TOKENS_MISMATCH);
+    }
+
+    function _toAssetManagerArray(ConstructorArgs memory args) private pure returns (address[] memory) {
+        // We assign the same asset manager to both the main and wrapped tokens.
+        address[] memory assetManagers = new address[](2);
+        assetManagers[0] = args.assetManager;
+        assetManagers[1] = args.assetManager;
+
+        return assetManagers;
+    }
+
+    function _getWrappedTokenRate() internal view override returns (uint256) {
+        try _cToken.exchangeRateStored() returns (uint256 rate) {
+            // exchangeRateStored returns the exchange rate scaled by 1e18, so no additional
+            // operations are needed here.
+            return rate;
+        } catch (bytes memory revertData) {
+            // By maliciously reverting here, any contract in the call stack could trick the Pool into
+            // reporting invalid data to the query mechanism for swaps/joins/exits.
+            // We then check the revert data to ensure this doesn't occur.
+            ExternalCallLib.bubbleUpNonMaliciousRevert(revertData);
+        }
+    }
+}
